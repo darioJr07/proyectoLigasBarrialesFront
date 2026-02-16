b9e22ebdcf136dab921b94c9699cc0a352316cb8
unknown <boli_cc@hotmail.com>
2026-02-16 17:17:42 -0500

    arreglo ruta back

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://proyectoligasbarrialesback.onrender.com//api',
+  apiUrl: 'https://proyectoligasbarrialesback.onrender.com/api',
 };
